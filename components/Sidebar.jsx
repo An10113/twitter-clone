f34c7d9fd34ae476017faaec91c0021733388d1e
An10113 <antranthanh1011@gmail.com>
2023-05-01 20:37:08 +0700

    1

---
+++ components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div>
+        <div>
+            Logo
+        </div>
+        <nav>
+            <ul>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+            </ul>
+        </nav>
+        <div>
+            Users
+        </div>
+    </div>
+
+  )
+}
